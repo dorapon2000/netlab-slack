c4fddcf7cf78c38598e16508cd4a72b9d26bce10
Kota Endo <ekotap07@gmail.com>
2019-09-30 02:25:56 +0900

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
-const env = process.env;
 
 const { App } = require('@slack/bolt');
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: env.SLACK_BOT_TOKEN,
-  signingSecret: env.SLACK_SIGNING_SECRET
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-app.message('knock knock', ({ message, say }) => {
-    say(`_Who's there?_`);
-});
+// Listens to incoming messages that contain "hello"
+app.message('hello', ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    say(`Hey there <@${message.user}>!`);
+  });
 
 (async () => {
   // Start your app
